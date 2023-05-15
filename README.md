6a820a570b0fb47e841999a39a58c45b5f5ae3fc
thiagofeldner <tfeldner@hotmail.com>
2023-05-15 10:02:04 -0300

    Primeiro commit do challenge alura

---
+++ README.md
@@ -0,0 +1,50 @@
+# Challenge Alura + Oracle
+## Decodificador de texto com Javascript
+
+<img src="./imgs/preview.png" width=550>
+
+Clique [AQUI](https://github.com/thiagofeldner/Decodificador-de-Texto) para acessar o projeto completo.
+<br>
+Clique [AQUI](https://thiagofeldner.github.io/Decodificador-de-Texto/) para acessar a página.
+
+<br>
+
+> ## 📝 Sobre o desafio
+<br>
+
+Desenvolver uma aplicação que criptografa textos.
+
+<br>
+
+🔑 As "chaves" de criptografia que utilizadas foram:
+- A letra "e" é convertida para "enter"
+- A letra "i" é convertida para "imes"
+- A letra "a" é convertida para "ai"
+- A letra "o" é convertida para "ober"
+- A letra "u" é convertida para "ufat"
+
+<br>
+
+✅ Requisitos:
+
+- Deve funcionar apenas com letras minúsculas
+- Não devem ser utilizados letras com acentos nem caracteres especiais
+- Deve ser possível converter uma palavra para a versão criptografada e também retornar uma palavra criptografada para a versão original
+
+<br>
+
+✨ Extra:
+- Um botão que copie o texto criptografado
+- Link das redes sociais
+
+<br>
+
+> ## 🖥️ Tecnologias
+<br>
+
+- HTML5
+- CSS3
+- JavaScript
+- Figma
+- Ion Icons
+- Google Fonts
---
+++ main.js
@@ -0,0 +1,75 @@
+/* Troca cor de fundo */
+
+function toggleMode() {
+  const html = document.documentElement;
+
+  html.classList.toggle('light');
+
+  const img = document.querySelector('.logo');
+  const alt = document.querySelector('.logo');
+
+  if(html.classList.contains('light')){
+    img.setAttribute('src', './imgs/logo.png');
+    alt.setAttribute('alt', 'Logo Alura azul');
+  } else {
+    img.setAttribute('src', './imgs/logo_branco.png')
+    alt.setAttribute('alt', 'Logo Alura cinza');
+  }
+}
+
+/*Codificar texto*/
+
+const textArea = document.querySelector(".texto");
+const msg = document.querySelector(".msg");
+
+function btnCriptografar() {
+  const textoCript = criptografar(textArea.value);
+  msg.value = textoCript;
+  textArea.value = "";
+}
+
+function criptografar(stringCriptografada){
+  let matriz = [
+    ['e' , 'enter'],
+    ['i', 'imes'],
+    ['a', 'ai'],
+    ['o', 'ober'],
+    ['u', 'ufat']
+  ];
+
+  stringCriptografada = stringCriptografada.toLowerCase();
+
+  for(let i = 0; i < matriz.length; i++) {
+    if(stringCriptografada.includes(matriz[i][0])){
+      stringCriptografada = stringCriptografada.replaceAll(matriz[i][0], matriz[i][1]);
+    }
+  }
+  return stringCriptografada;
+}
+
+/*Descodificar texto*/
+
+function btnDescriptografar() {
+  const textoDescript = descriptografar(textArea.value);
+  msg.value = textoDescript;
+  textArea.value = "";
+}
+
+function descriptografar(stringDescriptografada){
+  let matriz = [
+    ['e' , 'enter'],
+    ['i', 'imes'],
+    ['a', 'ai'],
+    ['o', 'ober'],
+    ['u', 'ufat']
+  ];
+
+  stringDescriptografada = stringDescriptografada.toLowerCase();
+
+  for(let i = 0; i < matriz.length; i++) {
+    if(stringDescriptografada.includes(matriz[i][1])){
+      stringDescriptografada = stringDescriptografada.replaceAll(matriz[i][1], matriz[i][0]);
+    }
+  }
+  return stringDescriptografada;
+}
